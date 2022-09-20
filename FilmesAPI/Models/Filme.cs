960188597f74bf4f79c73d4d65dc611d38f023b5
Kauane Farias <kauane.farias@wedecode.co>
2022-09-20 15:40:26 -0300

    @kaufariax [Task: Estrutura inicial da controller] {20/09/2022}

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo de título é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo de diretor é obrigatório")]
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        [Range(1, 300, ErrorMessage = "A duração deve ser entre 1 minuto e 5 horas")]
+        public int Duracao { get; set; }
+    }
+}

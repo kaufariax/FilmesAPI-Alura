e8105889e2084143d433f0ab30d4735433b5233d
Kauane Farias <kauane.farias@wedecode.co>
2022-09-20 16:10:44 -0300

    @kaufariax [Task: Conexão com banco de Dados local] {20/09/2022}

--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -1,7 +1,11 @@
+using FilmesAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<FilmeContext>(opts => opts.UseMySQL(builder.Configuration.GetConnectionString("FilmeConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

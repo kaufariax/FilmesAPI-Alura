13e270d6eae39fac088dd965e218d5ef7a013172
Kauane Farias <kauane.farias@wedecode.co>
2022-10-19 08:42:52 -0300

    @kaufariax [Task: Aprimorando a conexão com o banco de dados] {19/10/2022}

--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -5,7 +5,7 @@ using System.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<FilmeContext>(opts => opts.UseMySQL(builder.Configuration.GetConnectionString("FilmeConnection")));
+builder.Services.AddDbContext<FilmeContext>(opts => opts.UseMySql(builder.Configuration.GetConnectionString("filmeConection"), new MySqlServerVersion(new Version(8, 0))));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

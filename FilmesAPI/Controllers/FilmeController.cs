a730188da3360ae6eefe3f22297ef266be17c29b
Kauane Farias <kauane.farias@wedecode.co>
2022-10-24 09:10:47 -0300

    @kaufariax [Task: Mapeamento utilizando AutoMapper] {24/10/2022}

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesAPI.Data;
+﻿using AutoMapper;
+using FilmesAPI.Data;
 using FilmesAPI.Models;
 using FilmesAPI.Models.Dtos;
 using Microsoft.AspNetCore.Mvc;
@@ -11,23 +12,18 @@ namespace FilmesAPI.Controllers
     public class FilmeController : ControllerBase
     {
         private FilmeContext _context;
+        private IMapper _mapper;
 
-        public FilmeController(FilmeContext context)
+        public FilmeController(FilmeContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
         [HttpPost]
         public IActionResult AdicionaFilme([FromBody] CriarFilmeDto filmeDto)
         {
-            Filme filme = new Filme
-            {
-                Titulo = filmeDto.Titulo,
-                Diretor = filmeDto.Diretor,
-                Genero = filmeDto.Genero,
-                Duracao = filmeDto.Duracao
-            };
-
+            Filme filme = _mapper.Map<Filme>(filmeDto);
             _context.Filmes.Add(filme);
             _context.SaveChanges();
             return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
@@ -45,15 +41,7 @@ namespace FilmesAPI.Controllers
             Filme? filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
             if (filme != null)
             {
-                RetornoFilmeDto filmeDto = new RetornoFilmeDto
-                {
-                    Id = filme.Id,
-                    Titulo = filme.Titulo,
-                    Diretor = filme.Diretor,
-                    Genero = filme.Genero,
-                    Duracao = filme.Duracao,
-                    HoraDaConsulta = DateTime.Now
-                };
+                RetornoFilmeDto filmeDto = _mapper.Map<RetornoFilmeDto>(filme);
 
                 return Ok(filmeDto);
             }
@@ -68,10 +56,7 @@ namespace FilmesAPI.Controllers
             {
                 return NotFound();
             }
-            filme.Titulo = filmeDto.Titulo;
-            filme.Genero = filmeDto.Genero;
-            filme.Duracao = filmeDto.Duracao;
-            filme.Diretor = filmeDto.Diretor;
+            _mapper.Map(filmeDto, filme);
             _context.SaveChanges();
             return NoContent();
         }
---
+++ FilmesAPI/Models/FilmeProfile/FilmeProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FilmesAPI.Models.Dtos;
+
+namespace FilmesAPI.Models.Profiles
+{
+    public class FilmeProfile : Profile
+    {
+        public FilmeProfile()
+        {
+            CreateMap<CriarFilmeDto, Filme>();
+            CreateMap<Filme, RetornoFilmeDto>();
+            CreateMap<AtualizarFilmeDto, Filme>();
+        }
+    }
+}
